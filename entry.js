549e3b45e77566fa037408142d9608ae1665812b
Tim Doherty <tim.doherty@agilysys.com>
2016-05-19 09:54:23 -0700

    remove double quotes; update content for consistency

--- entry.js
+++ entry.js
@@ -1,3 +1,3 @@
-require("./style.css");
-document.write('<div id="main">Main Content</div><div id="split-content"></div>');
-require("./content.js");
+require('./style.css');
+document.write('<div id='main'><a href='#'>home</a> : <a href='#about'>about</a><hr /><div class='logo'></div><div id='split-content'></div></div>');
+require('./content.js');
