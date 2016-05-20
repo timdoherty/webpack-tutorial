ce5a4d8a84cc6d643e9799102133b14e0c7ae987
Tim Doherty <tim.doherty@agilysys.com>
2016-05-20 15:30:52 -0700

    formatting

--- entry.js
+++ entry.js
@@ -1,3 +1,2 @@
 require('./style.css');
-document.write('<div id="main"><a href="#">home</a> : <a href="#about">about</a><hr /><div class="logo"></div><div id="split-content"></div></div>');
 require('./content.js');
